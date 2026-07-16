ed56491fc4c223f30d13958e2b9b86e8dfa8892b
Alex Inkiläinen <alexinkilainen@gmail.com>
2026-07-16 09:59:00 +0300

    Fixed element ID

--- src/gui/js/main.js
+++ src/gui/js/main.js
@@ -116,7 +116,7 @@ async function applyLocalization(localization) {
     "span[data-localize='interior']": "interior",
     "span[data-localize='fillground']": "fillground",
     "span[data-localize='legacy_trees']": "legacy_trees",
-    "span[data-localize='random_roof_shapes']": "random_roof_shapes",
+    "span[data-localize='random_roof']": "random_roof_shapes",
     "span[data-localize='proposed_highways']": "proposed_highways",
     "span[data-localize='overture']": "overture",
     "span[data-localize='three_dmr']": "three_dmr",
@@ -1614,7 +1614,7 @@ async function startGeneration() {
     var interior = document.getElementById("interior-toggle").checked;
     var fill_ground = document.getElementById("fillground-toggle").checked;
     var legacy_trees = document.getElementById("legacy-trees-toggle").checked;
-    var random_roof_shapes = document.getElementById("random-roof-shapes-toggle").checked;
+    var random_roof_shapes = document.getElementById("random-roof-toggle").checked;
     var overture = document.getElementById("overture-toggle").checked;
     var proposed_highways = document.getElementById("proposed-highways-toggle").checked;
     var use_3d = document.getElementById("use-3d-toggle").checked;
